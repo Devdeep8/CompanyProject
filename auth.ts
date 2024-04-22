f5b95827c4f0b4b50081e03d9e36b0780feb0599
Devdeep Patidar <patidardevdeep999@gmail.com>
2024-04-22 23:59:10 +0530

    someerror

--- auth.ts
+++ auth.ts
@@ -1,11 +1,14 @@
+// auth.config.ts
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
-import { authConfig } from './auth.config';
+import GitHubProvider from 'next-auth/providers/github';
+import { authConfig } from './auth.config'; // Define authConfig separately
+import type { User } from '@/app/lib/definitions';
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
-import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
- 
+
+// Function to fetch user from the database based on email
 async function getUser(email: string): Promise<User | undefined> {
   try {
     const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
@@ -15,16 +18,16 @@ async function getUser(email: string): Promise<User | undefined> {
     throw new Error('Failed to fetch user.');
   }
 }
- 
+
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
   providers: [
     Credentials({
       async authorize(credentials) {
+        // Schema validation for credentials
         const parsedCredentials = z
           .object({ email: z.string().email(), password: z.string().min(6) })
           .safeParse(credentials);
- 
         if (parsedCredentials.success) {
           const { email, password } = parsedCredentials.data;
           const user = await getUser(email);
@@ -32,10 +35,15 @@ export const { auth, signIn, signOut } = NextAuth({
           const passwordsMatch = await bcrypt.compare(password, user.password);
           if (passwordsMatch) return user;
         }
- 
+
         console.log('Invalid credentials');
         return null;
-      }, 
+      },
     }),
+    GitHubProvider({
+      clientId: "e6be489afc9e3ba3649f",
+      clientSecret: "2fa9478317bfb33504a5250e485d25299598b230",
+    }),
+    // Add other providers as needed
   ],
 });
